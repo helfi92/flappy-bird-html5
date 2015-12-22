7dd36aca5e384d63e9919a865eb86067d88e6830
Hassan Ali <helfi92@gmail.com>
2015-12-21 20:41:16 -0500

    add more impl and implement onpress

--- app/components/home/landingPageController.js
+++ app/components/home/landingPageController.js
@@ -1,6 +1,11 @@
 app.controller('landingPageController',['$scope','spriteService', function($scope,spriteService){
 	
-	spriteService.init();
+	
+	spriteService.Sprite.prototype.draw = function(ctx, x , y){
+			ctx.drawImage(this.img, this.x, this.y, this.width, this.height,
+				x, y, this.width, this.height);
+	};
+	sprite = spriteService;
 
 	var
 	//game vars
@@ -64,11 +69,127 @@ app.controller('landingPageController',['$scope','spriteService', function($scop
 			ctx.restore();
 		}
 
+	};
+	function onpress(evt) {
+
+		switch (currentstate) {
+
+			// change state and update bird velocity
+			case states.Splash:
+				currentstate = states.Game;
+				bird.jump();
+				break;
+
+			// update bird velocity
+			case states.Game:
+				bird.jump();
+				break;
+
+			// change state if event within okbtn bounding box
+			case states.Score:
+				// get event position
+				var mx = evt.offsetX, my = evt.offsetY;
+
+				if (mx == null || my == null) {
+					mx = evt.touches[0].clientX;
+					my = evt.touches[0].clientY;
+				}
+
+				// check if within
+				if (okbtn.x < mx && mx < okbtn.x + okbtn.width &&
+					okbtn.y < my && my < okbtn.y + okbtn.height
+				) {
+					pipes.reset();
+					currentstate = states.Splash;
+					score = 0;
+				}
+				break;
+
+		}
+	}
+
+	//start and init game
+	function main() {
+		// create canvas and set width/height
+		canvas = document.createElement("canvas");
+
+		width = window.innerWidth;
+		height = window.innerHeight;
+
+		var evt = "touchstart";
+		if (width >= 500) {
+			width  = 320;
+			height = 480;
+			canvas.style.border = "1px solid #000";
+			evt = "mousedown";
+		}
+
+		// listen for input event
+		document.addEventListener(evt, onpress);
+
+		canvas.width = width;
+		canvas.height = height;
+		if (!(!!canvas.getContext && canvas.getContext("2d"))) {
+			alert("Your browser doesn't support HTML5, please update to latest version");
+		}
+		ctx = canvas.getContext("2d");
+
+		currentstate = states.Splash;
+		// append canvas to document
+		document.body.appendChild(canvas);
+
+		// initate graphics and okbtn
+		var img = new Image();
+		img.onload = function() {
+			console.log('sprite is: ', sprite);
+			sprite.initSprites(img);
+			ctx.fillStyle = s_bg.color;
+
+			okbtn = {
+				x: (width - s_buttons.Ok.width)/2,
+				y: height - 200,
+				width: s_buttons.Ok.width,
+				height: s_buttons.Ok.height
+			}
+
+			run();
+		}
+		img.src = "assets/img/sheet.png";
+	}
+	//start and update game loop
+	function run() {
+		var loop = function() {
+			update();
+			render();
+			window.requestAnimationFrame(loop, canvas);
+		}
+		window.requestAnimationFrame(loop, canvas);
+	}
+
+	//update foreground,pipe and bird
+	function update() {
+		frames++;
+
+		if (currentstate !== states.Score) {
+			fgpos = (fgpos - 2) % 14;
+		} else {
+			// set best score to maximum score
+			best = Math.max(best, score);
+			localStorage.setItem("best", best);
+		}
+		if (currentstate === states.Game) {
+			pipes.update();
+		}
+
+		bird.update();
+	}	
+
+	function render(){
+
 	}
 
 
-
-
+	main();
 
 
 }]);
--- app/components/services/spriteService.js
+++ app/components/services/spriteService.js
@@ -1,38 +1,98 @@
 app.factory('spriteService', function() {
   	return {
-  		init : function(){
-  			//sprite vars
-			var 
-			s_bird, 
-			s_bg, 
-			s_fg, 
-			s_pipeNorth,
-			s_pipeSouth,
-			s_text,
-			s_score,
-			s_splash,
-			s_buttons,
-			s_numberS,
-			s_numberB;	
+  		//sprite vars
+  		s_bird:null,
+		s_bg:null,
+		s_fg:null, 
+		s_pipeNorth:null,
+		s_pipeSouth:null,
+		s_text:null,
+		s_score:null,
+		s_splash:null,
+		s_buttons:null,
+		s_numberS:null,
+		s_numberB:null,
+  		
+  		Sprite : function(img, x, y , width, height){
+  			this.img = img;
+			this.x = x * 2;
+			this.y = y * 2
+			this.width = width * 2;
+			this.height = height * 2;
+  		},
 
-			//sprite class
-			function Sprite(img, x, y, width, height){
-				this.img = img;
-				this.x = x * 2;
-				this.y = y * 2
-				this.width = width * 2;
-				this.height = height * 2;	
-			};
 
-			//draw sprite to canvas
-			Sprite.prototype.draw = function(ctx, x , y){
-				ctx.drawImage(this.img, this.x, this.y, this.width, this.height,
-					x, y, this.width, this.height);
-			};
 
-			//init sprites
-  		}
+		initSprites : function(img){
+
+			s_bird = [
+				new this.Sprite(img, 156, 115, 17, 12),
+				new this.Sprite(img, 156, 128, 17, 12),
+				new this.Sprite(img, 156, 141, 17, 12)
+			];
+			
+			s_bg = new this.Sprite(img,   0, 0, 138, 114);
+			s_bg.color = "#70C5CF"; // save background color
+			s_fg = new this.Sprite(img, 138, 0, 112,  56);
+			
+			s_pipeNorth = new this.Sprite(img, 251, 0, 26, 200);
+			s_pipeSouth = new this.Sprite(img, 277, 0, 26, 200);
+			
+			s_text = {
+				FlappyBird: new this.Sprite(img, 59, 114, 96, 22),
+				GameOver:   new this.Sprite(img, 59, 136, 94, 19),
+				GetReady:   new this.Sprite(img, 59, 155, 87, 22)
+			}
+			s_buttons = {
+				Rate:  new this.Sprite(img,  79, 177, 40, 14),
+				Menu:  new this.Sprite(img, 119, 177, 40, 14),
+				Share: new this.Sprite(img, 159, 177, 40, 14),
+				Score: new this.Sprite(img,  79, 191, 40, 14),
+				Ok:    new this.Sprite(img, 119, 191, 40, 14),
+				Start: new this.Sprite(img, 159, 191, 40, 14)
+			}
+
+			s_score  = new this.Sprite(img, 138,  56, 113, 58);
+			s_splash = new this.Sprite(img,   0, 114,  59, 49);
+
+			s_numberS = new this.Sprite(img, 0, 177, 6,  7);
+			s_numberB = new this.Sprite(img, 0, 188, 7, 10);
+
+			/**
+			 * Draw number to canvas
+			 * 
+			 * @param  {CanvasRenderingContext2D} ctx context used for drawing
+			 * @param  {number} x      x-position
+			 * @param  {number} y      y-position
+			 * @param  {number} num    number to draw
+			 * @param  {number} center center to offset from
+			 * @param  {number} offset padd text to draw right to left
+			 */
+			s_numberS.draw = s_numberB.draw = function(ctx, x, y, num, center, offset) {
+				num = num.toString();
+
+				var step = this.width + 2;
+				
+				if (center) {
+					x = center - (num.length*step-2)/2;
+				}
+				if (offset) {
+					x += step*(offset - num.length);
+				}
+
+				for (var i = 0, len = num.length; i < len; i++) {
+					var n = parseInt(num[i]);
+					ctx.drawImage(img, step*n, this.y, this.width, this.height,
+						x, y, this.width, this.height)
+					x += step;
+				}
+			}
+		
+		}
+
+
   	}
+  	
 
 
 });
