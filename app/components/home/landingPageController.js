95220f0cec4de4578a22df8808743ef8077108dc
Hassan Ali <helfi92@gmail.com>
2015-12-21 21:07:16 -0500

    add background

--- app/components/home/landingPageController.js
+++ app/components/home/landingPageController.js
@@ -301,7 +301,40 @@ app.controller('landingPageController',['$scope','spriteService', function($scop
 	}	
 
 	function render(){
+		ctx.fillRect(0, 0, width, height);
+		// draw background sprites
+		s_bg.draw(ctx, 0, height - s_bg.height);
+		s_bg.draw(ctx, s_bg.width, height - s_bg.height);
 
+		pipes.draw(ctx);
+		bird.draw(ctx);
+
+		// draw forground sprites
+		s_fg.draw(ctx, fgpos, height - s_fg.height);
+		s_fg.draw(ctx, fgpos+s_fg.width, height - s_fg.height);
+
+		var width2 = width/2; // center of canvas
+
+		if (currentstate === states.Splash) {
+			// draw splash text and sprite to canvas
+			s_splash.draw(ctx, width2 - s_splash.width/2, height - 300);
+			s_text.GetReady.draw(ctx, width2 - s_text.GetReady.width/2, height-380);
+
+		}
+		if (currentstate === states.Score) {
+			// draw gameover text and score board
+			s_text.GameOver.draw(ctx, width2 - s_text.GameOver.width/2, height-400);
+			s_score.draw(ctx, width2 - s_score.width/2, height-340);
+			s_buttons.Ok.draw(ctx, okbtn.x, okbtn.y);
+			// draw score and best inside the score board
+			s_numberS.draw(ctx, width2-47, height-304, score, null, 10);
+			s_numberS.draw(ctx, width2-47, height-262, best, null, 10);
+
+		} else {
+			// draw score to top of canvas
+			s_numberB.draw(ctx, null, 20, score, width2);
+
+		}
 	}
 
 
