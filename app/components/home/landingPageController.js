2a8b63f92b1271f723f765ea5d845fd3820a8b02
Hassan Ali <helfi92@gmail.com>
2015-12-21 20:05:59 -0500

    add sprites and bird object with draw

--- app/components/home/landingPageController.js
+++ app/components/home/landingPageController.js
@@ -1,23 +1,74 @@
 app.controller('landingPageController',['$scope','spriteService', function($scope,spriteService){
-	//sprite vars
-	var 
-	s_bird, 
-	s_bg, 
-	s_fg, 
-	s_pipeNorth,
-	s_pipeSouth,
-	s_text,
-	s_score,
-	s_splash,
-	s_buttons,
-	s_numberS,
-	s_numberB;	
+	
+	spriteService.init();
 
-	function Sprite(img, x, y, width, height){
-		
+	var
+	//game vars
+	canvas,
+	ctx,
+	width,
+	height,
+
+	fgpos = 0,
+	frames = 0,
+	score = 0,
+	best = localStorage.getItem("best") || 0,
+
+	//state vars
+	currentstate,
+	states = {
+		Splash : 0,
+		Game : 1,
+		Score : 2
+	},
+
+	//game objects
+
+	//ok btn init in main()
+	okbtn,
+
+	bird = {
+		x: 60,
+		y: 0,
+
+		frame: 0,
+		velocity: 0,
+		animation : [0,1,2,1], //smooth transition of images from sprites
+
+		rotation: 0,
+		radius: 12,
+
+		gravity: 0.25,
+		_jump: 4.6,
+
+		jump : function(){
+			this.velocity = - this._jump;
+		},
+
+		//update sprite animation and position of bird
+		//===UNCOMPLETE
+		update: function(){
+
+		},
+		draw : function(ctx){
+			ctx.save();
+
+			ctx.translate(this.x, this.y);
+			ctx.rotate(this.rotation);
+
+			var n = this.animation[this.frame];
+
+			//draw bird with center in origo
+			s_bird[n].draw(ctx, -s_bird[n].width/2, -s_bird[n].height/2);
+
+			ctx.restore();
+		}
+
 	}
 
-	
-  	
-	return 0;
+
+
+
+
+
 }]);
--- app/components/services/spriteService.js
+++ app/components/services/spriteService.js
@@ -1,11 +1,38 @@
 app.factory('spriteService', function() {
-  	return{
-  		hello : 'hey',
+  	return {
+  		init : function(){
+  			//sprite vars
+			var 
+			s_bird, 
+			s_bg, 
+			s_fg, 
+			s_pipeNorth,
+			s_pipeSouth,
+			s_text,
+			s_score,
+			s_splash,
+			s_buttons,
+			s_numberS,
+			s_numberB;	
 
-  		yo : this.hello,
+			//sprite class
+			function Sprite(img, x, y, width, height){
+				this.img = img;
+				this.x = x * 2;
+				this.y = y * 2
+				this.width = width * 2;
+				this.height = height * 2;	
+			};
+
+			//draw sprite to canvas
+			Sprite.prototype.draw = function(ctx, x , y){
+				ctx.drawImage(this.img, this.x, this.y, this.width, this.height,
+					x, y, this.width, this.height);
+			};
+
+			//init sprites
+  		}
   	}
-
-  
 
 
 });
